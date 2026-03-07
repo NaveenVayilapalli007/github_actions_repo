e6f6a8c04b3ed71bcb88fdfe22497bea5dadf87b
Naveen Vayilapalli <naveenvayilapalli007@gmail.com>
2026-03-07 22:53:47 +0530

    terraform automation

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+aws_region  = "us-east-1"
+bucket_name = "terraform-demo-bucket-naveen-12345"
+environment = "dev"
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
